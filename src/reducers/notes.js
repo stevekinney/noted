37378c9757bd9336e01ec8c8dbc84fa62042de34
Steve Kinney <hello@stevekinney.net>
2018-05-30 03:23:41 -0500

    Clean up code

--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -2,7 +2,7 @@ const initialState =
   {
     1: {
       title: 'Things to Do',
-      body: '### To Do\n- Buy a winning lottery ticket\n- Buy some vegan spam\n-Clean out from under the bed'
+      body: '### To Do\n- Buy a winning lottery ticket\n- Buy some vegan spam\n- Clean out from under the bed'
     },
     2: {
       title: 'Very Important Link',
